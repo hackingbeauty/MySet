5729c53d37feaacd4762719bf5aec59676291865
Mark Muskardin <markmuskardin@gmail.com>
2011-03-26 16:01:02 -0700

    first files

---
+++ myset_spec.rb
@@ -0,0 +1,25 @@
+require "myset"
+
+describe MySet do
+  describe "#empty" do
+    it "should return true if the set is empty" do
+      @one = MySet.new
+      @one.should == 1
+    end
+  end
+  describe "#contains" do
+    it "should return true if the set contains a given item" do
+      
+    end
+  end
+  describe "#add" do
+    it "should successfully add an item to the set" do
+      
+    end
+  end
+  describe "#remove" do
+    it "should successfully remove an item from the set" do
+      
+    end
+  end
+end
