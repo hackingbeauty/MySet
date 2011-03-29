fdfbd1c758247398874502637143b60e09d0fa17
Mark Muskardin <markmuskardin@gmail.com>
2011-03-28 20:06:49 -0700

    successfully tested empty, contains, add, and size methods

--- myset.rb
+++ myset.rb
@@ -1,3 +1,39 @@
 class MySet
+  def initialize
+    @empty = true
+    @collection = []
+    @size = 0
+    @contains = false
+  end
+  
+  def empty?
+    @empty
+  end
+  
+  def add(item)
+    @collection.each do |col_item|
+      return if col_item == item
+    end
+    @empty = false
+    @size += 1
+    @collection[@size] = item
+  end
+  
+  def remove(item)
+    
+  end
+  
+  def size
+    @size
+  end
+  
+  def contains(item)
+    @collection.each do |col_item|
+      if col_item == item
+         @contains = true
+       end
+    end
+    @contains
+  end
   
 end
--- myset_spec.rb
+++ myset_spec.rb
@@ -1,24 +1,45 @@
 require "myset"
 
 describe MySet do
+  before(:each) do
+    @one = MySet.new
+    @two = MySet.new
+    @many = MySet.new
+  end
   describe "#empty" do
     it "should return true if the set is empty" do
-      @one = MySet.new
-      @one.should == 1
+      @one.empty?.should == true
+    end
+    it "should return false if the set is not empty" do
+      test = MySet.new
+      test.empty?.should_not be_false
     end
   end
   describe "#contains" do
     it "should return true if the set contains a given item" do
-      
+      @one.add("Liliana")
+      @one.contains("Liliana").should == true
     end
   end
   describe "#add" do
     it "should successfully add an item to the set" do
+      @one.add("Mark")
+      @one.empty?.should == false
+      @one.size.should == 1
       
+      @two.add("Mark")
+      @two.add("George")
+      @two.empty?.should == false
+      @two.size.should == 2
+    end
+    it "should not add an item already in the set" do
+      @many.add("Dario")
+      @many.add("Dario")
+      @many.size.should == 1
     end
   end
   describe "#remove" do
-    it "should successfully remove an item from the set" do
+    pending "should successfully remove an item from the set" do
       
     end
   end
